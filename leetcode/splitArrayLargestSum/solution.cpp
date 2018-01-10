731f16589c0d707c62945021a398f841e316ccb7
2011211242 <2990936748@qq.com>
2018-01-10 06:26:53 -0800

    add *

--- leetcode/splitArrayLargestSum/solution.cpp
+++ leetcode/splitArrayLargestSum/solution.cpp
@@ -1,36 +1,34 @@
 #include <cstdio>
 #include <climits>
 #include <vector>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    int binarySearch(vector<vector<int>> &mp, int s, int m)
-    {
-        int len = mp[s].size();
-        if(m == 1) return mp[s][len - 1];
-
-        int res = INT_MAX;
-        for(int i = 0; i <= len - m; i++) 
-        {
-            res = min(res, max(mp[s][i], binarySearch(mp, s + i + 1, m - 1))); 
-        }
-
-        return res;
-    }
-
     int splitArray(vector<int>& nums, int m) {
-        int len = nums.size();
-        vector<vector<int>> Map(len);
-        for(int i = 0; i < nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j < nums.size(); j++){
-                sum += nums[j];
-                Map[i].push_back(sum);
-            }
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 1; i <= n; i++){
+            dp[i][1] =  dp[i - 1][1] + nums[i - 1];
         }
-        return binarySearch(Map, 0, m);
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 2; j <= m && j <= i; j++){
+                int right = nums[i - 1];
+                dp[i][j] = max(dp[i - 1][j - 1], right);
+
+                for(int k = i - 2; k > 0 && right <= dp[i][j]; k--){
+                    right += nums[k];
+                    dp[i][j] = min(max(dp[k][j - 1], right), dp[i][j]);
+                }
+                //printf("%d\t", dp[i][j]);
+            }
+            //printf("\n");
+        }
+        return dp[n][m];
     }
 };
 
